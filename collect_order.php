237f7a0d1029558a698aebe9f8cf449fbf654c56
Ryan van Rooijen <aegisv.8@hotmail.co.za>
2019-01-31 21:32:41 +0200

    updated collect button to add and update items in inventory

--- collect_order.php
+++ collect_order.php
@@ -1,73 +1,45 @@
 <?php
 	require("settings.php");
-	
 	session_start();
-	
 	// header("Content-Type: application/json");
-
 	if(!isset($_SESSION["user"]))
 	{
 		header("Location: login.php");
 	}
-
-
 	$user = $_SESSION["user"];
 	$order_id = $_POST["orderNum"];
-	$rows =[];
 
 
+	$product_rows =[];
 	$query = "SELECT product_ref, quantity FROM order_product WHERE order_ref = {$order_id}";
-	$result = $db_con->query($query);
-	
-	while ($row = mysqli_fetch_array($result)) {
-		array_push($rows, $row);
-	}
-
-
-
-
-
-// $result = $mysqli->query("SELECT item_ref FROM mytable WHERE city = 'c7' LIMIT 1");
-// if($result->num_rows == 0) {
-//      // row not found, do stuff...
-// } else {
-//     // do other stuff...
-// }
-// $mysqli->close();
-
-
-
-
-				// die("item ref is not the same");
-	if($statement = $db_con->prepare("INSERT INTO user_inventory (inv_user_ref, item_ref, item_quantity) VALUES (?, ?, ?)"))
-	{
-		$statement->bind_param("iii", $user->id, $item_ref, $quantity);
-		foreach ($rows as $row) {
-			$row = (object)$row;
-			$item_ref = $row->product_ref;
-			$quantity = $row->quantity;
-			// $statement->execute();
-			$query = "SELECT item_ref FROM user_inventory WHERE item_ref = {$item_ref}";
-			$result = $db_con->query($query);
-			die(fetch($result));
-			$result = (object)$result;
-			if (!($result->item_ref == $item_ref || $result->item_ref == null || $result->item_ref == "")) {
-				die($result->item_ref);
-			}
-			else
-			{
-				die("item ref is not the same");
-			}
+	$product_result = $db_con->query($query);
+	while ($product_row = mysqli_fetch_array($product_result)) {
+		$query = "SELECT item_ref FROM user_inventory WHERE item_ref = {$product_row['product_ref']}";
+		$item_result = $db_con->query($query);
+		if (!($item_result->num_rows)) {
+			insertIntoTable($db_con, $user->id, $product_row['product_ref'], $product_row['quantity']);
+		}
+		else
+		{
+			updateTable($db_con, $user->id, $product_row['product_ref'], $product_row['quantity']);
 		}
 	}
-	else
+
+	function insertIntoTable($db_con, $user_id, $item_ref, $quantity)
 	{
-		// die("");
-		echo mysqli_error($db_con);
+		$statement = $db_con->prepare("INSERT INTO user_inventory (inv_user_ref, item_ref, item_quantity) VALUES (?, ?, ?)");
+		$statement->bind_param("iii", $user_id, $item_ref, $quantity);
+		$statement->execute();
+		$statement->close();
+		die("inserted into table");
 	}
 
-
-	// die("ssssss");
-
-	// die(json_encode($rows))
-	?>
+	function updateTable($db_con, $user_id, $item_ref, $quantity)
+	{
+		$statement = $db_con->prepare("UPDATE user_inventory SET item_quantity = item_quantity + ? WHERE item_ref = ?");
+		$statement->bind_param("ii", $quantity, $item_ref);
+		$statement->execute();
+		$statement->close();
+		die("updated table");
+	}
+?>
